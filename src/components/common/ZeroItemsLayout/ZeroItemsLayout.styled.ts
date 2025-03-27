d26f2a1c44f20b63bc8d75395bace987b67de956
00013540 <wiut00013540@gmail.com>
2025-03-28 03:26:11 +0500

    Fixed format

--- src/components/common/ZeroItemsLayout/ZeroItemsLayout.styled.ts
+++ src/components/common/ZeroItemsLayout/ZeroItemsLayout.styled.ts
@@ -2,8 +2,9 @@ import { Box, styled } from '@mui/material';
 
 const shouldForwardProp = (prop: string) => prop !== 'lightTheme';
 
-export const ZeroItemsLayoutWrapper = styled(Box, { shouldForwardProp })<{ lightTheme?: string }>(
-  ({ lightTheme }) => ({
+export const ZeroItemsLayoutWrapper = styled(Box, { shouldForwardProp })<{
+    lightTheme?: string;
+}>(({ lightTheme }) => ({
     padding: '2rem',
     flex: 1,
     display: 'flex',
@@ -15,16 +16,16 @@ export const ZeroItemsLayoutWrapper = styled(Box, { shouldForwardProp })<{ light
     maxWidth: '45em',
     margin: '0 auto',
     opacity: lightTheme === 'true' ? 1 : 0.8,
-  })
-);
+}));
 
-export const TypographyWrapper = styled(Box, { shouldForwardProp })<{ lightTheme?: string }>(
-  ({ theme, lightTheme }) => ({
+export const TypographyWrapper = styled(Box, { shouldForwardProp })<{
+    lightTheme?: string;
+}>(({ theme, lightTheme }) => ({
     padding: '1rem 1.5rem',
-    color: lightTheme === 'true' ? theme.palette.primary.contrastText : 'inherit',
+    color:
+        lightTheme === 'true' ? theme.palette.primary.contrastText : 'inherit',
 
     [theme.breakpoints.down('md')]: {
-      padding: '1rem 0.5rem',
+        padding: '1rem 0.5rem',
     },
-  })
-);
+}));
--- src/components/common/ZeroItemsLayout/ZeroItemsLayout.types.ts
+++ src/components/common/ZeroItemsLayout/ZeroItemsLayout.types.ts
@@ -1,11 +1,11 @@
 import type { ReactNode, ComponentType } from 'react';
 
 export interface ZeroItemsLayoutProps {
-  hint?: string;
-  desc?: string;
-  iconSize?: number;
-  className?: string;
-  lightTheme?: boolean;
-  children?: ReactNode;
-  Icon?: ComponentType;
+    hint?: string;
+    desc?: string;
+    iconSize?: number;
+    className?: string;
+    lightTheme?: boolean;
+    children?: ReactNode;
+    Icon?: ComponentType;
 }
